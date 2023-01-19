5feb833689477eacd427098d393bc3489fa6cc54
SShepeluk <98049987+SShepeluk@users.noreply.github.com>
2023-01-19 18:56:24 -0500

    Add files via upload
    Added javadoc code

--- main.java
+++ main.java
@@ -1,8 +1,34 @@
-//Sharlton Shepeluk
+/**
+ * 
+ * @author Sharlton Shepeluk
+ *
+ * Main method that calls a recursive static method that
+ * returns the nth term in the Fibonacci sequence.
+  */
+
+
 
 public class main {
 
-	// Recursive Fibonacci Method ================================================
+	
+	public static void main(String[] args) {
+
+		int n = 10; // Fibonacci method calculates nth number in sequence
+
+		int fibOutput = rFib(n);
+		System.out.println("The " + n + "th term in the Fibonacci sequence is " + fibOutput + ".");
+
+	}// end main
+
+/**
+ * 
+ * Recursive fibonacci method
+ * 	
+ * @param n a specified integer that tells the method what number in the Fib sequence
+ * 			to calculate for	 
+ * @return the integer at the nth number in the Fib sequence 
+ */
+	
 	public static int rFib(int n) {
 
 		if (n <= 1) {
@@ -11,16 +37,5 @@ public class main {
 			return rFib(n - 1) + rFib(n - 2);
 		}
 	}
-
-	// Main ======================================================================
-	public static void main(String[] args) {
-
-		int n = 10; // Fibonacci method calculates nth number in sequence
-
-		
-		int fibOutput = rFib(n);
-		System.out.println("The " + n + "th term in the Fibonacci sequence is " + fibOutput + ".");
-
-	}// end main
 
 }
